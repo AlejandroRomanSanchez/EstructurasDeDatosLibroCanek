2086d4900dd4dd0fc79fa98abab96715dc552cf5
Alejandro Roman Sanchez <alejandroromansanchez@ciencias.unam.mx>
2023-08-05 11:46:56 -0600

    Se agregó la clase PruebaEnteroComparable

---
+++ src/pruebas/PruebaEnteroComparable.java
@@ -0,0 +1,31 @@
+package pruebas;
+
+import clasesMejoradas.EnteroComparable;
+
+/**
+ * Esta clase sirve para hacer pruebas a la clase EnteroComparable
+ * @author Alejandro Román Sánchez
+ * @version 5 de agosto del 2023
+ * @since Libro de Canek
+ */
+public class PruebaEnteroComparable {
+    /**
+     * Este es el método main para la clase que nos permite probar a EnteroComparable
+     */
+    public static void main(String[ ] args) {
+	// Creamos los objetos que usaremos para hacer las pruebas
+	EnteroComparable entero1 = new EnteroComparable(5);
+	EnteroComparable entero2 = new EnteroComparable(11);
+	EnteroComparable entero3 = new EnteroComparable(11);
+
+	// Probamos el método getEntero( ) de la clase EnteroComparable
+	System.out.println("El entero1 es: " + entero1.getEntero( ));
+	System.out.println("El entero2 es: " + entero2.getEntero( ));
+	System.out.println("El entero3 es: " + entero3.getEntero( ));
+
+	// Probamos el método compareTo( ) de la clase EnteroComparable
+	System.out.println("\nEl entero1 es menor que el entero2: " + entero1.compareTo(entero2));
+	System.out.println("El entero2 es igual que el entero3: " + entero2.compareTo(entero3));
+	System.out.println("El entero2 es mayor que el entero1: " + entero2.compareTo(entero1));
+    }
+}
