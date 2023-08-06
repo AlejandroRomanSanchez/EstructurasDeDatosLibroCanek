b2a253566227e341691961a981c0921d4a227b10
Alejandro Roman Sanchez <alejandroromansanchez@ciencias.unam.mx>
2023-08-06 13:09:41 -0600

    Se agregó el método búsquedaBinaria(arreglo, elemento, comparador) de la clase Arreglo, además se corrigió el método creaSubArregloDe( ) de esa misma clase

--- src/clasesMejoradas/Arreglo.java
+++ src/clasesMejoradas/Arreglo.java
@@ -10,27 +10,82 @@ import java.util.Comparator;
  * @version 04 de agosto del 2023
  * @since Libro de Canek
  */
-public class Arreglo {    
+public class Arreglo {
+    /**
+     * Este método lleva a cabo una búsqueda binaria sobre el arreglo parámetro
+     * @param arreglo Es el arreglo en el cual se hará la búsqieda binaria
+     * @param elemento Es el elemento que se bsucará en el arreglo por medio de una búsqueda binaria
+     * @param comparador Este parámetro permite llevar a cabo comparaciones dentro del arreglo cuando
+     * se hace la búsqueda binaria
+     * @return Si el elemento a buscar se encuentra, se devuelve un índice del arreglo que guarda al elemento.
+     * En caso contrario, se devuelve el número -1
+     */
     public static <T> int busquedaBinaria(T[ ] arreglo, T elemento, Comparator<T> comparador) {
-	int longitudDeArreglo;
-	int puntoMedio;
+	/* En esta variable se hará referencia a la mitad del arreglo que posiblemente tenga al elemento que se
+	 * está buscando, por lo que se le aplicará el método busquedaBinaria
+	 */
+	T[ ] subArregloAIterar = null;
+	/* En esta variable se guardará el índice del arreglo que tiene al elemento, en caso de tenerlo. De lo
+	 * contrario, guardará al número -1
+	 */
+	int indiceBuscado = 0; 
+	int longitudDeArreglo; // Esta variable guardará la longitud del arreglo parámetro
+	int puntoMedio; // Es el punto medio de los índice del arreglo parámetro
 
-	if(arreglo == null || arreglo.length == 0) {
-	    return -1;
+	// Si null es el primer parámetro, lanzamos un NullPointerException
+	if(arreglo == null) {
+	    throw new NullPointerException("El primer parámetro es un null");
 	}
 
+	/* Como ya sabemos que el primer parámetro no es null, obtenemos la longitud del arreglo parámetro y el
+	 * punto medio de sus índices
+	 */
 	longitudDeArreglo = arreglo.length;
-	puntoMedio = longitudDeArreglo / 2;
+	puntoMedio = (longitudDeArreglo - 1) / 2;
+	
+	// Si el arreglo parámetro es vacío, devolvemos -1; pues no tiene al elemento que estamos buscando
+	if(longitudDeArreglo == 0) {
+	    indiceBuscado = -1;
+	    return indiceBuscado;
+	}
 
+	/* Si el arreglo no es vacío, hacemos comparaciones con respecto al elemento que estamos buscando y a lo
+	 * guardado en el punto medio de los índices del arreglo parámetro
+	 */
+
+	/* Si, según el comparador, el elemento que estamos buscando es exactamente igual a lo guardado en el
+	 * punto medio de los índices del arreglo parámetro, entonces devolvemos ese punto medio
+	 */
 	if(comparador.compare(elemento, arreglo[puntoMedio]) == 0) {
 	    return puntoMedio;
 	}
 
+	/* Si, según el comparador, el elemento que estamos buscando es menor que lo guardado en el punto
+	 * medio de los índices del arreglo parámetro, entonces hacemos una búsqueda binaria sobre el
+	 * sub-arreglo que corresponde a la mitad inferior del arreglo parámetro
+	 */
 	if(comparador.compare(elemento, arreglo[puntoMedio]) < 0) {
-	    
+	    subArregloAIterar = (T[ ]) Arreglo.creaSubArregloDe(arreglo, 0, puntoMedio - 1);
 	}
 
-	return 0;
+	/* Si, según el comparador, el elemento que estamos buscando es mayor que lo guardado en el punto
+	 * medio de los índices del arreglo parámetro, entonces hacemos una búsqueda binaria sobre el
+	 * sub-arreglo que corresponde a la mitad superior del arreglo parámetro
+	 */
+	if(comparador.compare(elemento, arreglo[puntoMedio]) > 0) {
+	    subArregloAIterar = (T[ ]) Arreglo.creaSubArregloDe(arreglo, puntoMedio + 1, longitudDeArreglo);
+	    /* Hacemos una conversión del índice del sub-arreglo al arreglo original, pues queremos obtener el
+	     * índice en términos del arreglo parámetro y no se su sub-arreglo
+	     */
+	    indiceBuscado += puntoMedio + 1; 
+	}
+
+	/* Hacemos una conversión del índice del sub-arreglo al arreglo origninal, pues queremos obtener el
+	 * índice en término del arreglo parámetro y no de sy sub-arreglo
+	 */
+	indiceBuscado += Arreglo.busquedaBinaria(subArregloAIterar, elemento, comparador);
+	
+	return indiceBuscado;
     }
 
     /**
@@ -41,47 +96,70 @@ public class Arreglo {
      * sea el primer elemento del sub-arreglo a crear
      * @param indiceSuperior Es el índice del arreglo parámetro del cual se tomará su elemento para que sea el
      * último elemento del sub-arreglo a crear
-     * @return Si el arreglo parámetro no es null y si los índices superior e inferior tienen sentido, entonces
-     * obtenemos un sub-arreglo del arreglo parḿaetro restringido a esos índices
+     * @return Si el arreglo parámetro no es null y si los índices superior e inferior tienen sentido en su
+     * orden, entonces obtenemos un sub-arreglo del arreglo parámetro restringido a esos índices. Si esto índice
+     * obtenemos un arreglo vacío. Si el índice inferior es menor que cero, se tomará como índice cero. Si el
+     * índice superior es mayor o igual que la longitud del arreglo, se tomará como el mayor índice que tiene
+     * el arreglo parámetro
      */
-    public static <T extends Comparable<T>> Comparable[ ] creaSubArregloDe(T[ ] arreglo, int indiceInferior,
-								 int indiceSuperior)	
-	throws Exception {
-	Comparable[ ] subArreglo; // En esta variable se construirá el sub-arreglo a devolver	
+    public static <T> Object[ ] creaSubArregloDe(T[ ] arreglo, int indiceInferior, int indiceSuperior) {
+	Object[ ] subArreglo = { }; // En esta variable se construirá el sub-arreglo a devolver
+	int longitudDeArreglo; // En esta variable se guardará a la longitud del arreglo parámetro
 	int longitudDeSubArreglo; // En esta variable se guardará la longitud del sub-arreglo a devolver
+	/* Esta variable originalmente guarda al índice inferior, pero si éste es menor que cero, entonces
+	 * guardaremos al número cero en esta variable
+	 */
+	int nuevoIndiceInferior = indiceInferior;
+	/* En esta variable originalmente guardamos al índice superior, pero si éste es mayor o igual que la
+	 * longitud del arreglo parámetro, entonces guardaremos al mayor de los índices del arreglo
+	 * parámetro en esta variable
+	 */
+	int nuevoIndiceSuperior = indiceSuperior;
 
 	// Si el primer argumento es null, lanzamos una excepción
 	if(arreglo == null) {
-	    throw new Exception("Null no debe ser el primer parámetro");
+	    throw new NullPointerException("El primer parámetro es null");
 	}
 
-	/* Si el arreglo parámetro no es null y alguno de los índices parámetros están fuera del rango de los
-	 * índices del arreglo, lanzamos una excepción
-	 */
-	if(indiceInferior < 0 || indiceSuperior >= arreglo.length) {
-	    throw new Exception("Alguno de los índices límites está fuera del rango del arreglo");
+	// Como el primer parámetro no es null, podemos guardar la longitud del arreglo parámetro
+	longitudDeArreglo = arreglo.length;
+
+	// Si el arreglo parámetro es vacío, entonces devolvemos un arreglo vacío
+	if(longitudDeArreglo == 0) {
+	    return subArreglo;
 	}
 
-	/* Si el arreglo parámetro no es null y si los índices de restricción están al revés, entonces lanzamos una
-	 * excepción
+	/* Si el arreglo parámetro no es null y si los índices de restricción están al revés, entonces devolvemos
+	 * un arreglo vacío
 	 */
-	if(indiceInferior > indiceSuperior) {
-	    throw new Exception("El índice inferior debe ser menor o igual que el indice superior");
+	if(indiceInferior > indiceSuperior) {	    
+	    return subArreglo;
 	}
 
-	/* Si el arreglo parámetro no es null y si los índices de restricción tienen sentido en su orden, procedemos
-	 * a crear el sub-arreglo
+	/* Si el índice inferior es menor que cero, entonces ahora consideraremos al cero como índice inferior
+	 * del rango del arreglo parámetro que será tomado para construir el sub-arreglo
 	 */
+	if(indiceInferior < 0) {
+	    nuevoIndiceInferior = 0;
+	}
+
+	/* Si el índice es mayor o igual que la longitud del arreglo parámetro, entonces ahora consideramos al
+	 * mayor de sus índice como el índice superior del rango del arreglo parámetro que será tomado para
+	 * construir el sub-arreglo
+	 */
+	if(indiceSuperior >= longitudDeArreglo) {
+	    nuevoIndiceSuperior = longitudDeArreglo - 1;
+	}
 
 	// Definimos la longitud del sub-arreglo que será devuelto
-	longitudDeSubArreglo = 1 + indiceSuperior - indiceInferior; 
-	subArreglo = new Comparable[longitudDeSubArreglo]; // Creamos el sub-arreglo que será devuelto
+	longitudDeSubArreglo = 1 + nuevoIndiceSuperior - nuevoIndiceInferior; 
+	subArreglo = new Object[longitudDeSubArreglo]; // Creamos el sub-arreglo que será devuelto
 
-	/* Guardamos los elementos del arreglo parámetro que van del primer índice de restricción al segundo índice
-	 * de restricción
+	/* Guardamos los elementos del arreglo parámetro que van del primer índice de restricción al segundo
+	 * índice de restricción
 	 */
 	for(int i = 0; i < longitudDeSubArreglo; i++) {
-	    subArreglo[i] = arreglo[indiceInferior + i];
+	    subArreglo[i] = arreglo[nuevoIndiceInferior + i];
 	}
 
 	return subArreglo;	
